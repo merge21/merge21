b618fbc6e87905d3a3f3bce7aa99073b849e1a55
bagas-sanjaya-UeU <charderrasanjaya@student.esaunggul.ac.id>
2023-11-28 22:06:09 +0700

    menambahkan navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import StickyNavbar from '../components/StickyNavbar'
+import { Button } from "@material-tailwind/react";
+
+
+
+export default function Home() {
+  return (
+    <div>
+        <StickyNavbar />
+        <Button>Button</Button>
+    </div>
+  )
+}
